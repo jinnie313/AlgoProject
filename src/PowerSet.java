c85463866426ec657954363614e74b6f80938217
student <student@DESKTOP>
2019-02-13 14:24:53 +0900

    부분집합 기본 & 부분집합 재귀

--- src/PowerSet.java
+++ src/PowerSet.java
@@ -3,8 +3,13 @@ public class PowerSet {
 
 	public static void main(String[] args) {
 		int arr[] = {3, 6, 7, 1, 5, 4};
+//		method1(arr);
+		method2(arr);
+	}
+
+	public static void method1(int[] arr) {
 		int n = arr.length;
-		
+
 		for(int i=0; i<(1<<n); i++) {
 			// 1<<n : 부분 집합의 개수
 			for(int j=0; j<n; j++) {
@@ -15,5 +20,31 @@ public class PowerSet {
 			}
 			System.out.println();
 		}
+	}
+
+	static boolean[] flag;
+	public static void method2(int[] arr) {
+		flag = new boolean[arr.length];
+		powerset(arr, 0, arr.length);
+
+	}
+
+	public static void powerset(int[] arr, int depth, int maxSize) {
+		if(depth == maxSize) {
+			print(arr);
+			return;
+		}
+
+		flag[depth] = true;
+		powerset(arr, depth+1, maxSize);
+		flag[depth] = false;
+		powerset(arr, depth+1, maxSize);
+	}
+
+	public static void print(int[] arr) {
+		for(int a: arr) {
+			System.out.print(a+" ");
+		}
+		System.out.println();
 	}
 }
