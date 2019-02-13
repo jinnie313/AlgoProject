2737aaad750832db3178791557def5d5a6236ce3
student <student@DESKTOP>
2019-02-13 14:01:04 +0900

    순열 기본!

---
+++ src/PermTest.java
@@ -0,0 +1,36 @@
+
+public class PermTest {
+	static int count=0;
+	public static void main(String[] args) {
+		char[] arr = {'A', 'B', 'C', 'D'};
+		perm(arr, 0);
+		
+	}
+	
+	public static void perm(char[] arr, int pivot) {
+		if(pivot == arr.length) {
+			pr(arr);
+			return;
+		}
+		
+		for(int i=pivot; i<arr.length; i++) {
+			System.out.println("i: " + i+"      pivot: " +pivot);
+			swap(arr, i, pivot);
+			perm(arr, pivot+1);
+			swap(arr, i, pivot);
+		}
+	}
+	
+	public static void swap(char[] arr, int x, int y) {
+		char temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
+	
+	public static void pr(char[] arr) {
+		for(int i=0; i<arr.length-1; i++) {
+			System.out.print(arr[i]+",");
+		}
+		System.out.println(arr[arr.length-1]);
+	}
+}
