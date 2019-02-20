98f5e0dddcbacb39d59328bdf07de8f794d55eb8
김채은 <jinnie313@naver.com>
2019-02-20 11:40:54 +0900

    반복문을 이용한 DFS

--- src/DFSTestRepeat.java
+++ src/DFSTestRepeat.java
@@ -1,11 +1,14 @@
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
-public class DFSTestRecursive {
+import DFSTestRecursive.Graph;
+
+public class DFSTestRepeat {
 	public static void main(String[] args) {
 		//그래프 생성
 		Graph g = new Graph(7+1);
-		
+
 		//간선추가
 		g.addEdge(1, 2);
 		g.addEdge(1, 3);
@@ -16,37 +19,45 @@ public class DFSTestRecursive {
 		g.addEdge(6, 7);
 		g.addEdge(3, 7);
 		
-		g.dfs(1);
+		g.search(1);
 	}
-	
+
 	static class Graph{
-		int v;
-		LinkedList<Integer> adj[];
+		Stack<Integer> stack;
 		boolean[] visit;
-		
+		LinkedList<Integer> adj[];
+
 		public Graph(int v) {
-			this.v = v;
+			stack = new Stack<>();
 			visit = new boolean[v];
 			adj = new LinkedList[v];
 			for(int i=1; i<v; i++) {
 				adj[i] = new LinkedList<>();
 			}
 		}
-		
+
 		public void addEdge(int i, int j) {
 			adj[i].add(j);
 			adj[j].add(i);
 		}
-		
-		public void dfs(int start) {
-			visit[start] = true;
-			System.out.print(start + " ");
+
+		public void search(int start) {
+			stack.push(start);
 			
-			Iterator<Integer> list = adj[start].iterator();
-			while(list.hasNext()) {
-				int next = list.next();
-				if(!visit[next]) {
-					dfs(next);
+			int v, next;
+			while(!stack.isEmpty()) {
+				v = stack.pop();
+				if(!visit[v]) {
+					visit[v] = true;
+					System.out.print(v +" ");
+					
+					Iterator<Integer> list = adj[v].iterator();
+					while (list.hasNext()) {
+						next = list.next();
+						if(!visit[next]) {
+							stack.push(next);
+						}
+					}
 				}
 			}
 		}
